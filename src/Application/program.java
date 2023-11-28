20c0f85da67ab9c3882ea6c19a261c6ab49191f5
Gabriel Zancanella Pereira <gabrielzancanella35@gmail.com>
2023-11-28 11:58:44 -0300

    Creation of SellerDaoJDBC and FactoryDao Classes
    This commit includes the implementation of the SellerDaoJDBC and FactoryDao classes. The SellerDaoJDBC class is responsible for the concrete implementation of data access operations for sellers using JDBC, while FactoryDao is a factory for creating instances of specific DAOs, providing flexibility in choosing the implementation.

--- src/Application/program.java
+++ src/Application/program.java
@@ -2,6 +2,8 @@ package Application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,6 +13,8 @@ public class program {
 
 		Department obj = new Department(1, "Books");
 		Seller obj2 = new Seller(1, "Frank Castle","frank.castle@gmail.com", new Date(),2050.55, obj);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(obj2);	
 	}
