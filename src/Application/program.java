791f1297b589d2f7e12dd93e28f36958049aa207
Gabriel Zancanella Pereira <gabrielzancanella35@gmail.com>
2023-11-28 11:42:08 -0300

    Creation of Seller Class
    This commit adds the Seller class to the project. The Seller class has been created to represent specific information about sellers, providing a structure that will facilitate the manipulation and organization of data related to each seller in the system.

--- src/Application/program.java
+++ src/Application/program.java
@@ -1,15 +1,17 @@
 package Application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class program {
 
 	public static void main(String[] args) {
 
 		Department obj = new Department(1, "Books");
+		Seller obj2 = new Seller(1, "Frank Castle","frank.castle@gmail.com", new Date(),2050.55, obj);
 		
-		System.out.println(obj);
-		
+		System.out.println(obj2);	
 	}
-
 }
--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -6,13 +6,13 @@ public class Department implements Serializable{
 	private static final long serialVersionUID = 1L;
 
 	private Integer id;
-	private String name;
+	private String  name;
 	
 	public Department() {
 	}
 	
 	public Department(Integer id, String name) {
-		this.id = id;
+		this.id   = id;
 		this.name = name;
 	}
 
@@ -35,8 +35,8 @@ public class Department implements Serializable{
 	@Override
 	public int hashCode() {
 		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		int result      = 1;
+		result          = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
 
