4f48b98bc1e397d783529b299c7096347d2c3b80
Gabriel Zancanella Pereira <gabrielzancanella35@gmail.com>
2023-11-29 08:55:01 -0300

    Implementation of findById
    In this commit, the implementation of the findById method has been added to the SellerDaoJDBC class. This method enables the retrieval of sellers by ID from the database, contributing to the complete data access functionality for the Seller entity.

--- src/Application/program.java
+++ src/Application/program.java
@@ -1,21 +1,17 @@
 package Application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class program {
 
 	public static void main(String[] args) {
 
-		Department obj = new Department(1, "Books");
-		Seller obj2 = new Seller(1, "Frank Castle","frank.castle@gmail.com", new Date(),2050.55, obj);
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		System.out.println(obj2);	
+		Seller seller = sellerDao.findById(7);
+		
+		System.out.println(seller);	
 	}
 }
