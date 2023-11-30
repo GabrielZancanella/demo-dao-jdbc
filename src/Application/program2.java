a5df862dba39b2fd1e9d6206e2dcfccf45c216bd
Gabriel Zancanella Pereira <gabrielzancanella35@gmail.com>
2023-11-30 09:37:51 -0300

    Improvement of Program2
    In this commit, the Program2 has been improved to make tests clearer and more concise, introducing a small system for handling departments. This enhancement provides a more effective understanding of the tested functionalities.

--- src/Application/program2.java
+++ src/Application/program2.java
@@ -2,6 +2,7 @@ package Application;
 
 import java.util.List;
 import java.util.Scanner;
+
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
@@ -10,41 +11,95 @@ public class program2 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("\n=== START DEPARTMENT TESTS ==");
+
+		int operation = 0;
+		int id = 0;
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
-		
-		System.out.println("=== TEST 1: department findById ===");
-		Department department = departmentDao.findById(1);
-		System.out.println(department);	
-		
-		
-		System.out.println("\n=== TEST 2: department findAll ===");
-		List<Department> list2 = departmentDao.findAll();
-		
-		for (Department department2 : list2) {
-			System.out.println(department2);	
+		Department department = null;
+		Department newDepartment = null;
+		boolean executingSystem = true;
+		List<Department> allDepartments = null;
+		String departmentName = "";
+		String oldDepartmentName = "";
+
+		while (executingSystem) {
+			System.out.println("\n=== MENU DEPARTMENT ===");
+			System.out.println("1 - Find by ID.");
+			System.out.println("2 - Find All.");
+			System.out.println("3 - Department Insert.");
+			System.out.println("4 - Department Update.");
+			System.out.println("5 - Department Delete.");
+			System.out.println("6 - Exit system.");
+
+			System.out.print("\nChose one operation: ");
+			operation = sc.nextInt();
+
+			while (operation < 1 || operation > 6) {
+				System.out.print("\nChose a valid operation! ");
+				System.out.print("\nChose one operation: ");
+				operation = sc.nextInt();
+			}
+
+			switch (operation) {
+			case 1:
+				System.out.print("\nID to be searched for: ");
+				id = sc.nextInt();
+				department = departmentDao.findById(id);
+				if (department != null) {
+					System.out.println(department);
+					department = null;
+				} else
+					System.out.println("This ID not exists!");
+
+				System.out.println("=======================");
+				break;
+			case 2:
+				System.out.println("\nAll Departments:");
+				allDepartments = departmentDao.findAll();
+
+				for (Department allDepartmentsAux : allDepartments) {
+					System.out.println(allDepartmentsAux);
+				}
+				System.out.println("=======================");
+				break;
+			case 3:
+				System.out.print("\nEnter the Department name: ");
+				sc.nextLine();
+				departmentName = sc.nextLine();
+				newDepartment = new Department(null, departmentName);
+				departmentDao.insert(newDepartment);
+				System.out.println("Inserted! New ID =" + newDepartment.getId());
+				System.out.println("=======================");
+				break;
+			case 4:
+				System.out.print("\nID to be updated for: ");
+				id = sc.nextInt();
+				department = departmentDao.findById(id);
+				if (department != null) {
+					System.out.print("\nEnter the Department name to be updated: ");
+					sc.nextLine();
+					departmentName = sc.nextLine();
+					oldDepartmentName = department.getName();
+					department.setName(departmentName);
+					departmentDao.update(department);
+					System.out.printf("Updated! \n   Old name: %s\n   New name: %s", oldDepartmentName,
+							department.getName());
+				} else
+					System.out.println("This id not exists!");
+				System.out.println("=======================");
+				break;
+			case 5:
+				System.out.println("\nEnter the ID to be Deleted for: ");
+				id = sc.nextInt();
+				departmentDao.deleteById(id);
+				System.out.println("Deleted!");
+				System.out.println("=======================");
+				break;
+			case 6:
+				executingSystem = false;
+				break;
+			}
 		}
-		
-		System.out.println("\n=== TEST 3: department insert ===");
-		Department newDepartment = new Department(null, "Inserted");
-		departmentDao.insert(newDepartment);
-		System.out.println("Inserted! New id ="+ newDepartment.getId());
-		
-		
-		System.out.println("\n=== TEST 4: department update ===");
-		department = departmentDao.findById(9);
-		department.setName("Sports");
-		departmentDao.update(department);
-		System.out.println("Updated! "+ department);
-		
-		
-		System.out.println("\n=== TEST 6: department delete ===");
-		int id = sc.nextInt();
-		departmentDao.deleteById(id);
-		System.out.println("Deleted!");
-		System.out.println("\n=== END DEPARTMENT TESTS ==");
-		
 		sc.close();
 	}
 
